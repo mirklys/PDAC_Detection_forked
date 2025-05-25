79ce8dcfa39ebf49041c51466ac4f3f78d3f8f31
Giedrius Mirklys <giedriusmirklys8@gmail.com>
2025-05-25 20:04:02 +0000

    added testing and docker

---
+++ Dockerfile
@@ -0,0 +1,81 @@
+#FROM --platform=linux/amd64 pytorch/pytorch
+FROM nvidia/cuda:12.2.0-runtime-ubuntu20.04 AS base
+
+# Use a 'large' base container to show-case how to load pytorch and use the GPU (when enabled)
+
+# Ensures that Python output to stdout/stderr is not buffered: prevents missing information when terminating
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONWARNINGS="ignore"
+
+
+RUN apt-get update && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:deadsnakes/ppa && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    git \
+    wget \
+    unzip \
+    libopenblas-dev \
+    python3.9 \
+    python3.9-dev \
+    python3-pip \
+    nano \
+    && \
+    apt-get clean autoclean && \
+    apt-get autoremove -y && \
+    rm -rf /var/lib/apt/lists/* 
+
+RUN groupadd -r user && useradd -m --no-log-init -r -g user user
+
+RUN mkdir -p /opt/algorithm
+RUN chown -R user /opt/algorithm
+ENV PATH="/home/user/.local/bin:${PATH}"
+
+USER user
+
+COPY --chown=user:user requirements.txt /opt/app/
+
+# You can add any Python dependencies to requirements.txt
+RUN python3.9 -m pip install \
+    --user \
+    --no-cache-dir \
+    --no-color \
+    --requirement /opt/app/requirements.txt
+
+
+### Clone nnUNet
+# Configure Git, clone the repository without checking out, then checkout the specific commit
+RUN git config --global advice.detachedHead false && \
+    git clone https://github.com/MIC-DKFZ/nnUNet.git /opt/algorithm/nnunet/ 
+
+# Install a few dependencies that are not automatically installed
+RUN pip3 install \
+    -e /opt/algorithm/nnunet \
+    graphviz \
+    onnx \
+    SimpleITK && \
+    rm -rf ~/.cache/pip
+
+COPY --chown=user:user ./packages/report-guided-annotation/ /opt/algorithm/report-guided-annotation/
+
+# Install 'report-guided-annotation' in editable mode
+RUN cd /opt/algorithm/report-guided-annotation && \
+    python3.9 -m pip install --user --no-cache-dir -e . && \
+    cd /
+
+RUN mkdir -p /opt/algorithm/nnunet/nnunetv2/training/nnUNetTrainer/variants/loss/
+COPY --chown=user:user ./packages/nnunetv2/nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCELoss.py /opt/algorithm/nnunet/nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerCELoss.py
+
+COPY --chown=user:user ./workspace/nnUNet_results/ /opt/algorithm/nnunet/nnUNet_results/
+
+### Define workdir
+WORKDIR /opt/app
+COPY --chown=user:user ./main.py /opt/app/main.py
+
+RUN rm -rf ~/.cache/pip
+### Set environment variable defaults
+ENV nnUNet_raw="/opt/algorithm/nnunet/nnUNet_raw" \
+    nnUNet_preprocessed="/opt/algorithm/nnunet/nnUNet_preprocessed" \
+    nnUNet_results="/opt/algorithm/nnunet/nnUNet_results"
+
+ENTRYPOINT [ "python3.9", "-m", "main"]
---
+++ test.py
@@ -0,0 +1,103 @@
+import json
+import os
+import subprocess
+
+ROOT_FOLDER = "/home/bobby/repos/PDAC_Detection_forked/"
+TEST_FOLDER = os.path.join(ROOT_FOLDER, "workspace/test")
+
+os.makedirs(TEST_FOLDER, exist_ok=True)
+os.makedirs(os.path.join(TEST_FOLDER, "imagesTs"), exist_ok=True)
+os.makedirs(os.path.join(TEST_FOLDER, "labelsTs"), exist_ok=True)
+os.makedirs(os.path.join(TEST_FOLDER, "labelsTs/predicted"), exist_ok=True)
+
+
+def get_testing_fold(fold: int) -> list:
+    SPLITS_PATH = "./workspace/nnUNet_preprocessed/splits_final.json"
+    with open(SPLITS_PATH, "r") as f:
+        folds = json.load(f)
+
+    testing_fold = folds[fold].get("val", [])
+    assert len(testing_fold) > 0, "No validation data found for the specified fold."
+
+    return testing_fold
+
+
+def get_images(data_folder: str, fold: int = 0) -> list:
+    """Gets all the testing images/labels from the specified folder."""
+    listing = os.listdir(data_folder)
+    testing_fold = get_testing_fold(fold)
+
+    if not listing:
+        raise ValueError(f"The directory {data_folder} is empty or does not exist.")
+
+    prefixes = tuple(testing_fold)
+    images = [
+        os.path.join(data_folder, file) for file in listing if file.startswith(prefixes)
+    ]
+
+    if not images:
+        raise ValueError(f"No images found in {data_folder} for the specified fold.")
+    return images
+
+
+def move_images_to_test(images: list):
+    for image in images:
+        image_name = os.path.basename(image)
+        new_image_path = os.path.join(TEST_FOLDER, "imagesTs", image_name)
+        if not os.path.exists(new_image_path):
+            os.symlink(image, new_image_path)
+
+
+def create_tester_shell_script(
+    test_folder: str, env_vars: dict, output_file: str = "test.sh"
+) -> None:
+    """Creates a shell script to run the testing process."""
+    with open(output_file, "w") as f:
+        f.write(
+            """#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --account=cseduIMC037
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=15G
+#SBATCH --time=12:00:00
+#SBATCH --output=logs/preprocess-%j.out
+#SBATCH --error=logs/preprocess-%j.err
+#SBATCH --mail-user=giedrius.mirklys@ru.nl
+#SBATCH --mail-type=END,FAIL
+
+CONDA_BASE_DIR=$(conda info --base)
+source "${CONDA_BASE_DIR}/etc/profile.d/conda.sh"
+conda activate pdac_tversky \n""" ### WRITE YOUR ENVIRONEMNT
+        )
+        f.write(f"export nnUNet_raw={env_vars['nnUNet_raw']}\n")
+        f.write(f"export nnUNet_preprocessed={env_vars['nnUNet_preprocessed']}\n")
+        f.write(f"export nnUNet_results={env_vars['nnUNet_results']}\n")
+        f.write(
+            f"export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS={env_vars['ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS']}\n"
+        )
+        f.write(f"export OMP_NUM_THREADS={env_vars['OMP_NUM_THREADS']}\n")
+        f.write("\n")
+        f.write(f"echo 'RAW=' $nnUNet_raw\n")
+        f.write(
+            f"""python -m main -i {test_folder}/imagesTs/ -o {test_folder}/labelsTs/predicted \n"""
+        )
+    os.chmod(output_file, 0o777)
+
+
+def run_test(env_vars: dict, data_folder: str, bash_test_name: str):
+    images = get_images(data_folder)
+    move_images_to_test(images)
+    create_tester_shell_script(TEST_FOLDER, env_vars, output_file=bash_test_name)
+    subprocess.run([os.path.join(ROOT_FOLDER, bash_test_name)])
+
+if __name__ == "__main__":
+    env_vars = {
+        "nnUNet_raw": "./workspace/nnUNet_preprocessed",
+        "nnUNet_preprocessed": "./workspace/nnUNet_preprocessed",
+        "nnUNet_results": "./workspace/nnUNet_results",
+        "ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS": "2",
+        "OMP_NUM_THREADS": "2",
+    }
+    data_folder = os.path.join(ROOT_FOLDER, "test/imagesTs")
+    bash_test_name = "baseline_test.sh"
+    run_test(data_folder=data_folder, env_vars=env_vars, bash_test_name=bash_test_name)
